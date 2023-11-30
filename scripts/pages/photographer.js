91bc68538d79af94ed9e40c964127aa298cdc8c0
ThomasBfrd <thomas.bfrd@gmail.com>
2023-11-30 18:19:43 +0100

    Début page Photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,31 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const getIdByUrl = () => {
+  const url = new URL(document.location);
+  const id = parseInt(url.searchParams.get("id"));
+
+  return id;
+};
+
+async function getPhotographers() {
+  const reponse = await fetch("../../data/photographers.json");
+  const photographers = await reponse.json();
+
+  return photographers;
+}
+
+async function displayData(photographers) {
+  const id = getIdByUrl();
+  const photographer = photographers.filter((user) => user.id === id)[0];
+
+  const photographerHeader = document.querySelector(".photograph_header");
+  const photographerModel = photographerTemplate(photographer);
+  const userCardDOM = photographerModel.getUserCardDOM();
+  photographerHeader.appendChild(userCardDOM);
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
