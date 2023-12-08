ac02c54c7c907a9baff6e37a0599a1ef3b82dcb7
ThomasBfrd <thomas.bfrd@gmail.com>
2023-12-08 13:53:22 +0100

    mise en page page photographer w/ medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,17 +8,16 @@ class PhotographerProfile {
     const $article = document.createElement("article");
 
     const photographersCard = `
-        <a href="/photographer.html/${this._data._id}">
-            <div class="imgUser">
-            <img src="${this._data.portrait}" alt="" class="portrait_${this._data._id}">
-            </div>
-            <h2 class="name">${this._data.name}</h2>
-        </a>
-        <div class="textContent">
-            <p class="city">${this._data.city}, ${this._data.country}</p>
-            <p class="tagline">${this._data.tagline}</p>
-            <p class="price">${this._data.price}€/jour</p>
-        </div>
+    <div class="textContent">
+    <h2 class="name">${this._data.name}</h2>
+    <p class="city">${this._data.city}, ${this._data.country}</p>
+    <p class="tagline">${this._data.tagline}</p>
+    <p class="price">${this._data.price}€/jour</p>
+    </div>
+    <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    <div class="imgUser">
+    <img src="${this._data.portrait}" alt="" class="portrait_${this._data._id}">
+    </div>
        `;
 
     $article.innerHTML = photographersCard;
